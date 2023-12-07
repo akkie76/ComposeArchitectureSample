15a93c44f28274c0d77928ff6369e9462edcee64
akihiyo76 <ak.sato.info@gmail.com>
2023-12-07 16:45:28 +0900

    remove file

--- app/src/main/java/jp/co/compose/architecture/sample/domain/repository/module/action/RepositoryActionCreator.kt
+++
@@ -1,8 +0,0 @@
-package jp.co.compose.architecture.sample.domain.repository.module.action
-
-import android.app.Activity
-
-interface RepositoryActionCreator {
-
-    fun launchBrowser(activity: Activity, url: String)
-}
--- app/src/main/java/jp/co/compose/architecture/sample/domain/repository/usecase/ProvideBrowserUseCase.kt
+++
@@ -1,8 +0,0 @@
-package jp.co.compose.architecture.sample.domain.repository.usecase
-
-import android.app.Activity
-
-interface ProvideBrowserUseCase {
-
-    fun launch(activity: Activity, url: String)
-}
